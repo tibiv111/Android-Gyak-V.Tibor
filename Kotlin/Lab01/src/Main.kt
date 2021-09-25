20ed8c70af3d42abe0d5d3f027ab660f419e84d8
Váncza Tibor <59438148+tibiv111@users.noreply.github.com>
2021-09-25 13:21:34 +0300

    Elvégzett labor 1.
    Feladatok jól elkülönítve láthatóak.

--- Kotlin/Lab01/src/Main.kt
+++ Kotlin/Lab01/src/Main.kt
@@ -1,31 +1,99 @@
 package fundamentals
 
+
 import java.util.*
+import kotlin.random.Random.Default.nextInt
+import java.util.Random
 
 fun main()
 {
-    println("Hello");
-    val daysOfWeek : List<String> = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+    println("\n-----1. Feladat------\n")
+    fel1()
+
+    println("\n-----2. Feladat------\n")
+
+    val daysOfWeek : List<String> = listOf("monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     for(item in daysOfWeek)
     {
         println(item)
     }
 
-    //aysOfWeek.forEach {day -> println(day)}
+    fel2(daysOfWeek)
+    //daysOfWeek.forEach {day -> println(day)}
 
     //daysOfWeek.forEach {println(it)}
 
 
-    //fel1()
-    //fel2(daysOfWeek)
+
+    println("\n-----3. Feladat------\n")
+    println("Prime numbers: ")
     for (i in 2..100)
     {
+
         val k = fel3(i);
-        println( "$i = $k")
+        if (k)
+        {
+            print( "$i ")
+        }
+
 
     }
-    encode("Hi there. How are you?");
-    decode("SGkgdGhlcmUuIEhvdyBhcmUgeW91Pw==");
+    println("\n-----4. Feladat------\n")
+    val encodedStringMain = messageCoding("Hi there. How are you?", ::encode)
+    messageCoding(encodedStringMain, ::decode)
+
+    //encode("Hi there. How are you?");
+    //decode("SGkgdGhlcmUuIEhvdyBhcmUgeW91Pw==");
+    println("\n-----5. Feladat------\n")
+
+    val sourceList = mutableListOf<Int>()
+    for (i in 0..100)
+    {
+        sourceList.add(i);
+    }
+    sourceList.filter { even(it) }.forEach{print("$it ")}
+
+    println("\n-----6. Feladat------")
+
+    println("\n--Double of list")
+    sourceList.map { double(it) }.forEach{print("$it ")}
+    println("\n--Uppercase")
+    daysOfWeek.map { it.uppercase() }.forEach { print("$it ") }
+    println("\n--Capitalize")
+    daysOfWeek.map { it.capitalize()}.forEach { print("$it ") }
+    println("\n--Length of days")
+    daysOfWeek.map { it.length }.forEach { print("$it ") }
+    println("\n--Average length of days")
+    println(daysOfWeek.map { it.length }.average())
+
+    println("\n-----7. Feladat------\n")
+
+    print(daysOfWeek.toMutableList().filter { !it.contains("n") })
+    println("\n")
+    daysOfWeek.toMutableList().filter { !it.contains("n") }.forEachIndexed{ index, it -> println("Item at $index is $it")}
+    println()
+    print(daysOfWeek.toMutableList().filter { !it.contains("n") }.sorted())
+
+
+    println("\n-----8. Feladat------\n")
+
+    println("--Numbers")
+    var numbers = (1..10).map { nextInt(0, 100) } //irhattam volna egy sorba, de nem akartam
+    numbers.forEach{ print("$it\n")}
+    println("--Sorted")
+    println(numbers.sorted())
+    println("--Doesn't have even numbers?")
+    println(numbers.filter { even(it) }.isEmpty())
+    println("--All numbers are even?")
+    println(numbers.filter { even(it) }.size == numbers.size)
+    println("--Average of numbers")
+    //ezt a feladatot nem értem. A foreach az listák iterálására való. Egyelemű listát kiíratni felesleges foreach-el
+    println(numbers.average())
+
+
+
+
 
 }
 
@@ -40,8 +108,11 @@ fun fel1()
 
 fun fel2(daysOfWeek:List<String>)
 {
+    println("\n--Starts with 'T'")
     daysOfWeek.filter{it.startsWith('T')}.forEach{println(it)}
+    println("\n--Contains 'e'")
     daysOfWeek.filter{it.contains('e')}.forEach{println(it)}
+    println("\n--weekday length = 6")
     daysOfWeek.filter { it.length == 6  }.forEach{println(it)}
 }
 
@@ -60,19 +131,33 @@ fun fel3(n : Int) : Boolean
     return p == 1
 }
 
-fun encode(oriString:String)
+
+
+
+fun messageCoding(msg: String, func: (String) -> String): String
+{
+    return func(msg)
+}
+
+
+fun encode(oriString:String) : String
 {
     val encodedString: String = Base64.getEncoder().encodeToString(oriString.toByteArray())
     println(encodedString)
+    return encodedString
 
 }
 
-fun decode(encodedString:String)
+fun decode(encodedString:String) : String
 {
     val decodedBytes = Base64.getDecoder().decode(encodedString)
     val decodedString = String(decodedBytes)
     println(decodedString)
+    return decodedString
 
 }
+fun even(x: Int):Boolean = (x % 2 == 1)
+fun double(x: Int):Int = x * 2
+
 
 
